5ee8e5824bd627b387ef8978f523bece271c0c4c
Jeffrey <Jefferymsd101@gmail.com>
2026-06-28 21:22:46 +0530

    Implement user registration

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,48 @@
 const express = require("express");
-const app = express(); 
+const mongoose  = require("mongoose")
+const app = express();
+const User = require("./models/User")
+const bcrypt = require("bcryptjs")
+
 app.use(express.json()); 
 
 app.get("/",(req,res)=>{ 
     res.send("Hello user")
 });
+
+app.post("/register", async (req,res) => {
+  try{
+    const{username, email, password} = req.body;
+    if(username === "" || email === "" || password === ""){
+      return res.send("Please fill all fields")
+    }
+
+    const existingUsername = await User.findOne({username});
+    if(existingUsername){
+      return res.send("Username already exists")
+    }
+    const existingEmail = await User.findOne({email})
+    if(existingEmail){
+      return res.send("Email already exists")
+    }
+
+    const hashedPassword = await bcrypt.hash(password,10)
+    const user = {
+       username,
+       email,
+       password: hashedPassword 
+    }
+    console.log(user)
+
+    await User.create(user)
+    res.send("User registered successfully")
+  }
+  catch(error){
+    console.log(error)
+    res.send("Something went wrong")
+  }   
+
+})
 
 mongoose.connect(process.env.MONGO_URI) 
 .then(() => { 
